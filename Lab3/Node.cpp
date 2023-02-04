8cd48a7e9581be0afac3885caf166eb8f36c3fae
TeeTyJunGz <68283753+TeeTyJunGz@users.noreply.github.com>
2023-02-04 19:33:12 +0700

    Lab3/Node.h

---
+++ Lab3/Node.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include "Node.h"
+
+using namespace std;
+
+Node::Node(){
+    value = 0;
+    next = NULL;
+}
+
+Node::Node(int newValue, Node* newNext){
+    value = newValue;
+    next = newNext;
+}
+ 
+// Driver's code
+int main()
+{
+
+ 
+    return 0;
+}
+ 
